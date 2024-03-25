c0da72ac4a7b17bbc17e84741f0e6c16d590b750
Eliezer <eliezerTerrero12@gmail.com>
2024-03-25 16:34:49 -0400

    Creando el services de Empleado en el client

---
+++ FinalProject.Client/Services/EmpleadoServices.cs
@@ -0,0 +1,44 @@
+﻿using Shared.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace FinalProject.Client.Services
+{
+    public class EmpleadoServices(HttpClient httpClient)
+    {
+        public async Task<List<Empleados>?> GetEmpleados()
+        {
+            return await httpClient
+                 .GetFromJsonAsync<List<Empleados>>("api/Empleados");
+        }
+
+        public async Task<Empleados?> GetEmpleado(int id)
+        {
+            return await httpClient
+                .GetFromJsonAsync<Empleados>($"api/Empleados/{id}");
+        }
+        public async Task<Empleados?> Save(Empleados empleado)
+        {
+            var empleadoGuardado = await httpClient
+                .PostAsJsonAsync("api/Empleados", empleado);
+            if (!empleadoGuardado.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            return await empleadoGuardado.Content.ReadFromJsonAsync<Empleados>();
+        }
+        public async Task Delete(int id)
+        {
+            await httpClient.DeleteAsync($"api/Empleados/{id}");
+        }
+        public async Task<bool> Edit(int id, Empleados empleado)
+        {
+            var EmpleadoEditado = await httpClient.PutAsJsonAsync($"api/Empleados/{id}", empleado);
+            if (!EmpleadoEditado.IsSuccessStatusCode)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
